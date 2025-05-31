ad62a3ee6af078539b92919d4cf6a8e9a9087311
SamoKopecky <sano.kopecky@gmail.com>
2025-05-31 20:28:17 +0200

    fix: Remove vue zoom lib
    Signed-off-by: SamoKopecky <sano.kopecky@gmail.com>

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -52,78 +52,72 @@ function selectMachine(machine: Machine) {
   }
   activeMachine.value = machine
 }
-
-const closeMenuDebounce = useDebounceFn(() => {
-  showMenu.value = false
-}, 50)
 </script>
 
 <template>
   <div>
     <MachineInfo v-model="isOpen" :machine="activeMachine" />
-    <VueZoomable selector="#panable" @panned="closeMenuDebounce">
-      <div>
-        <v-menu
-          v-if="activeMachine"
-          v-model="showMenu"
-          :activator="`#${activeMachine.htmlId}`"
-          :close-on-content-click="false"
-          offset="10"
-          :open-on-click="false"
-          location="left"
-          persistent
-        >
-          <v-card min-width="150" max-width="300">
-            <v-card-title>
-              <div class="d-flex align-center justify-space-between">
-                <p>{{ activeMachine.name }}</p>
-                <v-btn
-                  @click="showMenu = false"
-                  class="ml-4"
-                  color="error"
-                  variant="text"
-                  icon="mdi-close"
-                ></v-btn>
-              </div>
-            </v-card-title>
-            <v-card-subtitle> {{ activeMachine.muscleGroups.join(", ") }} </v-card-subtitle>
-            <v-card-text>
-              <v-spacer></v-spacer>
-              <v-btn @click="isOpen = true">More details</v-btn>
-            </v-card-text>
-          </v-card>
-        </v-menu>
-      </div>
-      <svg
-        id="panable"
-        width="900"
-        height="1206"
-        view-box="0 0 900 1206"
-        style="background-color: grey"
+    <div>
+      <v-menu
+        v-if="activeMachine"
+        v-model="showMenu"
+        :activator="`#${activeMachine.htmlId}`"
+        :close-on-content-click="false"
+        offset="10"
+        :open-on-click="false"
+        location="left"
+        persistent
       >
-        <image href="../assets/map.svg" x="0" y="0" width="100%" height="100%" />
-        <g v-for="machine in machines" :key="machine.name">
-          <rect
-            :id="machine.htmlId"
-            stroke="blue"
-            fill="#000000"
-            :x="machine.position.x"
-            :y="machine.position.y"
-            :width="machine.dimension.width"
-            :height="machine.dimension.height"
-            style="cursor: pointer"
-            @click="selectMachine(machine)"
-          />
-          <text
-            :x="machine.position.x + machine.dimension.width / 3"
-            :y="machine.position.y + machine.dimension.height / 2"
-            fill="red"
-            style="pointer-events: none"
-          >
-            {{ machine.name }}
-          </text>
-        </g>
-      </svg>
-    </VueZoomable>
+        <v-card min-width="150" max-width="300">
+          <v-card-title>
+            <div class="d-flex align-center justify-space-between">
+              <p>{{ activeMachine.name }}</p>
+              <v-btn
+                @click="showMenu = false"
+                class="ml-4"
+                color="error"
+                variant="text"
+                icon="mdi-close"
+              ></v-btn>
+            </div>
+          </v-card-title>
+          <v-card-subtitle> {{ activeMachine.muscleGroups.join(", ") }} </v-card-subtitle>
+          <v-card-text>
+            <v-spacer></v-spacer>
+            <v-btn @click="isOpen = true">More details</v-btn>
+          </v-card-text>
+        </v-card>
+      </v-menu>
+    </div>
+    <svg
+      id="panable"
+      width="900"
+      height="1206"
+      view-box="0 0 900 1206"
+      style="background-color: grey"
+    >
+      <image href="../assets/map.svg" x="0" y="0" width="100%" height="100%" />
+      <g v-for="machine in machines" :key="machine.name">
+        <rect
+          :id="machine.htmlId"
+          stroke="blue"
+          fill="#000000"
+          :x="machine.position.x"
+          :y="machine.position.y"
+          :width="machine.dimension.width"
+          :height="machine.dimension.height"
+          style="cursor: pointer"
+          @click="selectMachine(machine)"
+        />
+        <text
+          :x="machine.position.x + machine.dimension.width / 3"
+          :y="machine.position.y + machine.dimension.height / 2"
+          fill="red"
+          style="pointer-events: none"
+        >
+          {{ machine.name }}
+        </text>
+      </g>
+    </svg>
   </div>
 </template>
