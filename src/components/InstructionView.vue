4c64aaa7aee0734e98b32d2fc82aae91488b1119
SamoKopecky <sano.kopecky@gmail.com>
2025-06-17 21:17:54 +0200

    feat: Add permisiion check for instructions
    Signed-off-by: SamoKopecky <sano.kopecky@gmail.com>

--- src/components/InstructionView.vue
+++ src/components/InstructionView.vue
@@ -3,9 +3,15 @@ import { useUser } from "@/composables/useUser"
 import { instructionService } from "@/services/instruction"
 import { type Instruction } from "@/types/instruction"
 import { watchDebounced } from "@vueuse/core"
+import { computed } from "vue"
 
 const instruction = defineModel<Instruction>({ required: true })
-const { isTrainer } = useUser()
+const { isTrainer, userId, isAdmin } = useUser()
+const canEdit = computed(() => {
+  const trainerOwns = isTrainer.value && userId === instruction.value.user_id
+  // True if either train owns the instruction or its an admin
+  return trainerOwns || isAdmin.value
+})
 
 watchDebounced(
   () => instruction.value.description,
@@ -23,7 +29,7 @@ watchDebounced(
       variant="outlined"
       auto-grow
       v-model="instruction.description"
-      :readonly="!isTrainer"
+      :readonly="!canEdit"
     />
   </v-card>
 </template>
