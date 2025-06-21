70d3131b029feb5624964e1569155009de5d8fab
Samuel Kopecky <samuel.kopecky@heureka.group>
2025-06-21 09:15:20 +0200

    feat: Make admin page look nicer
    Signed-off-by: Samuel Kopecky <samuel.kopecky@heureka.group>

--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -12,7 +12,7 @@ const email = ref<string>()
 const tableLoading = ref(false)
 const emailLoading = ref(false)
 const isFormValid = ref(true)
-const confirmDeleteActve = ref(false)
+const confirmDeleteActive = ref(false)
 const unregisterUser = ref<User>()
 
 const { addNotification } = useNotificationStore()
@@ -52,11 +52,11 @@ function unregisterUserFn() {
       addNotification("User unregistered succesfuly", "success")
     })
     .catch(() => addNotification("User unregistration failed", "error"))
-    .finally(() => (confirmDeleteActve.value = false))
+    .finally(() => (confirmDeleteActive.value = false))
 }
 
 function confirmUserUnregistration(user: User) {
-  confirmDeleteActve.value = true
+  confirmDeleteActive.value = true
   unregisterUser.value = user
 }
 
@@ -68,64 +68,89 @@ onMounted(() => loadUsers())
 </script>
 
 <template>
-  <v-dialog v-model="confirmDeleteActve">
-    <!-- TODO: Make this look nice -->
-    <v-card max-width="500px" title="Are you sure?">
-      <template #text>
-        <v-btn color="green" @click="unregisterUserFn">
-          Yes I want to unregister {{ unregisterUserName }}</v-btn
-        >
-        <v-spacer />
-        <v-btn class="mt-2" color="red" @click="confirmDeleteActve = false">
-          No I don't want to unregister {{ unregisterUserName }}
+  <v-dialog v-model="confirmDeleteActive" max-width="500px">
+    <v-card>
+      <v-card-title class="text-h5 text-center">Are you sure?</v-card-title>
+      <v-card-text class="text-center">
+        Do you really want to unregister <strong>{{ unregisterUserName }}</strong
+        >? This action cannot be undone.
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue-darken-1" variant="text" @click="confirmDeleteActive = false">
+          Cancel
         </v-btn>
-      </template>
+        <v-btn color="red-darken-1" variant="tonal" @click="unregisterUserFn"> Unregister </v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 
-  <v-card title="Trainers" flat>
-    <template #text>
+  <v-card>
+    <v-card-title class="d-flex align-center">
+      <v-icon icon="mdi-account-group"></v-icon> &nbsp; Trainers
+
       <v-text-field
+        class="ml-3"
         v-model="search"
+        density="compact"
         label="Search"
         prepend-inner-icon="mdi-magnify"
         variant="outlined"
+        flat
         hide-details
         single-line
-      />
-      <v-data-table :loading="tableLoading" :headers="headers" :items="users" :search="search">
-        <!-- eslint-disable-next-line  -->
-        <template #item.actions="{ item }">
-          <v-btn
-            variant="text"
-            color="red"
-            icon="mdi-close"
-            @click="confirmUserUnregistration(item)"
-          />
-        </template>
-      </v-data-table>
+      ></v-text-field>
+    </v-card-title>
 
-      <v-form v-model="isFormValid" @submit.prevent>
-        <v-text-field
-          v-model="email"
-          :rules="emailRules"
-          variant="outlined"
-          hide-details="auto"
-          clearable
-          placeholder="Enter email of new trainer"
-          label="Email"
-          prepend-inner-icon="mdi-email"
-        />
+    <v-divider></v-divider>
 
+    <v-data-table :loading="tableLoading" :headers="headers" :items="users" :search="search">
+      <!-- eslint-disable-next-line -->
+      <template #item.actions="{ item }">
         <v-btn
-          class="mt-1"
-          :loading="emailLoading"
-          :disabled="!isFormValid"
-          color="green"
-          @click="addNew"
-          >Register new trainer</v-btn
-        >
+          variant="text"
+          color="grey-darken-1"
+          v-tooltip:bottom="'Unregister'"
+          icon="mdi-close-circle-outline"
+          @click="confirmUserUnregistration(item)"
+        ></v-btn>
+      </template>
+    </v-data-table>
+
+    <v-divider></v-divider>
+
+    <v-card-text>
+      <v-form v-model="isFormValid" @submit.prevent="addNew">
+        <v-row class="align-center">
+          <v-col cols="12" sm="8">
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              variant="outlined"
+              density="compact"
+              hide-details="auto"
+              clearable
+              placeholder="Enter email of new trainer"
+              label="New Trainer Email"
+              prepend-inner-icon="mdi-email-plus-outline"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <v-btn
+              :loading="emailLoading"
+              :disabled="!isFormValid"
+              color="primary"
+              type="submit"
+              block
+              variant="tonal"
+            >
+              <v-icon start icon="mdi-account-plus"></v-icon>
+              Register Trainer
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-form>
-    </template>
+    </v-card-text>
   </v-card>
 </template>
