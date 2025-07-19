f390465b4799ca843abda1077ad9f6b0a7019d25
SamoKopecky <sano.kopecky@gmail.com>
2025-07-19 11:12:55 +0200

    fix
    Signed-off-by: SamoKopecky <sano.kopecky@gmail.com>

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,5 @@
-import { globalIgnores } from "eslint/config"
 import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript"
-import pluginVue from "eslint-plugin-vue"
 import pluginVitest from "@vitest/eslint-plugin"
-import skipFormatting from "@vue/eslint-config-prettier/skip-formatting"
 
 export default defineConfigWithVueTs(
   {
@@ -10,14 +7,10 @@ export default defineConfigWithVueTs(
     files: ["**/*.{ts,mts,tsx,vue}"],
   },
 
-  globalIgnores(["**/dist/**", "**/dist-ssr/**", "**/coverage/**"]),
-
-  pluginVue.configs["flat/essential"],
   vueTsConfigs.recommended,
 
   {
     ...pluginVitest.configs.recommended,
     files: ["src/**/__tests__/*"],
   },
-  skipFormatting,
 )
